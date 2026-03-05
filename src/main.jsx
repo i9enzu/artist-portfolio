ba4ced70f518e187245cd6c5727c99fabcbae2ff
enzunine <i9enzu33@gmail.com>
2026-03-05 07:16:42 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { AppProvider } from './context/AppContext.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import './index.css'
+import App from './App.jsx'
+import { AppProvider } from './context/AppContext.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <AppProvider>
-              <App />
-              <Toaster position="bottom-right" />
-        </AppProvider>AppProvider>
-    </React.StrictMode>React.StrictMode>,
-  )</React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+        <Toaster
+          position="bottom-right"
+          toastOptions={{
+            style: {
+              background: '#27272a',
+              color: '#fafafa',
+              border: '1px solid #3f3f46',
+            },
+            success: {
+              iconTheme: { primary: '#d946ef', secondary: '#fafafa' },
+            },
+          }}
+        />
+      </AppProvider>
+    </BrowserRouter>
+  </StrictMode>,
+)
